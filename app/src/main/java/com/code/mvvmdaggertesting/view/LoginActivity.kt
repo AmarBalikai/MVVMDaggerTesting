489f04270ccdff861581bb4a7a7d2340ed119622
Amar Balikai <amar.balikai108@gmail.com>
2021-11-11 22:13:09 +0530

    Setup created for MVVM architecture.

---
+++ app/src/main/java/com/code/mvvmdaggertesting/view/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.code.mvvmdaggertesting.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.code.mvvmdaggertesting.R
+
+/**
+ * Activity for Login Flow.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
